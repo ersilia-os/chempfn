b3511b882439a5c9df7885614e6353787ed5da0f
dhanshreea <DhanshreeA@users.noreply.github.com>
2023-02-26 11:58:20 +0530

    WIP Refactoring

--- ensemble_tabpfn/samplers/features.py
+++ ensemble_tabpfn/samplers/features.py
@@ -1,7 +1,7 @@
 import numpy as np
 from lol import LOL
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.feature_selection import SelectKBest, chi2
 
 #TODO consolidate constants
@@ -10,12 +10,16 @@ N_FEATURES = 100
 
 
 class BaseSampler:
-
-    def __init__(self, n_features=N_FEATURES, fit_with_y=False) -> None:
-        self.n_features = n_features
+    def __init__(self, fit_with_y=False) -> None:
         self.fit_with_y = fit_with_y
+        self.sampler = None
+
+    def _validate_sampler(self) -> None:
+        if self.sampler is None:
+            raise NotImplementedError
 
     def sample(self, X, y):
+        self._validate_sampler()
         if self.fit_with_y:
             return self.sampler.fit_transform(X,y)
         else:
@@ -24,28 +28,39 @@ class BaseSampler:
 class SelectKSampler(BaseSampler):
 
     def __init__(self) -> None:
-        super().__init__(N_FEATURES, fit_with_y=True)
-        self.sampler = SelectKBest(chi2, k=self.n_features)
+        super().__init__(fit_with_y=True)
+        self.sampler = SelectKBest(chi2, k=N_FEATURES)
         
 class KMeansSampler(BaseSampler):
     def __init__(self) -> None:
-        super().__init__(N_FEATURES, fit_with_y=True)
-        self.sampler = KMeans(n_clusters=N_FEATURES)
+        super().__init__()
+        self.sampler = MiniBatchKMeans(n_clusters=N_FEATURES, random_state=42)
+
+    def sample(self, X, y):
+
+        # Treat samples as features because we want to generate clusters 
+        # in the feature space and not in the sample space which is what
+        # K Means is used for.
+        cluster_distances = self.sampler.fit_transform(X.T)
+
+        # Relevant feature indices
+        relevant = np.argmin(cluster_distances, axis=0)
+
 
 class LRPSampler(BaseSampler):
     def __init__(self) -> None:
-        super().__init__(N_FEATURES, fit_with_y=True)
-        self.sampler = LOL(n_components=self.n_features)
+        super().__init__(fit_with_y=True)
+        self.sampler = LOL(n_components=N_FEATURES)
 
 
 class PCASampler(BaseSampler):
     def __init__(self) -> None:
-        super().__init__(N_FEATURES, fit_with_y=False)
-        self.sampler = PCA(n_components=self.n_features)
+        super().__init__(fit_with_y=False)
+        self.sampler = PCA(n_components=N_FEATURES)
 
 class RandomSampler(BaseSampler):
     def __init__(self) -> None:
-        super().__init__(N_FEATURES, fit_with_y=False)
+        super().__init__(fit_with_y=False)
         self.is_fit = False
         self.indices = []
         
